d7b339545091c74d0c4dc698a7dced28b6334e2d
MatiasB235 <135553952+MatiasB235@users.noreply.github.com>
2023-07-06 15:50:05 -0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numeros [10]int
+
+	for i := 0; i < 10; i++ {
+		fmt.Print("Ingrese un número entero: ")
+		fmt.Scanln(&numeros[i])
+	}
+
+	min := buscarMinimo(numeros)
+	max := buscarMaximo(numeros)
+	promedio := calcularPromedio(numeros)
+	sumatoria := calcularSumatoria(numeros)
+
+	fmt.Println("Mínimo: ", min)
+	fmt.Println("Máximo: ", max)
+	fmt.Println("Promedio: ", promedio)
+	fmt.Println("Sumatoria: ", sumatoria)
+
+}
+
+func buscarMinimo(nums [10]int) (minimo int) {
+	minimo = nums[0]
+
+	for i := 1; i < 10; i++{
+		if nums[i] < minimo {
+			minimo = nums[i]
+		}
+	}	
+
+	return
+}
+
+func buscarMaximo(nums [10]int) (maximo int) {
+	maximo = nums[0]
+
+	for i := 1; i < 10; i++{
+		if nums[i] > maximo {
+		maximo=nums[i]
+		//actualizar el maximo
+		}
+	}	
+	//TODO completar
+	return
+}
+
+func calcularPromedio(nums [10]int)(promedio float64) {
+	
+ 	
+	//TODO completar
+	return
+}
+
+func calcularSumatoria(nums [10]int) (acumulador float64) {
+	
+	acumulador = 0.0
+ 	contador := 0
+ 	for i := 0; i < 10; i++ { 	
+	acumulador += float64(nums[i])
+	contador++
+	}
+	//TODO completar
+	return
+}
